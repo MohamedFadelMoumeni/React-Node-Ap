ccab2f495cfe638b4546e41c2260d5a95d411343
mohamedfadel2002 <mohamedfadelmoumeni@gmail.com>
2021-06-07 10:12:05 +0200

    adding base file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express'); //CommonJs
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
-   console.log(res.send());
-});
+const app = express();
+passport.use(new GoogleStrategy());
+
 
 const PORT = process.env.PORT || 5000; //use 5000 in our machine instead use the PORT that the cloud gives to us 
 app.listen(5000);
+// Client ID 707770149189-8ne0mk321ovem99qa5si7p0n9nhen67e.apps.googleusercontent.com
+// SECRET ID _YBVMOQ03OwDWzgBgBnqAone
