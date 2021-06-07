635e2b32935b1b248065edfae94d94df06cb02ff
MohamedFadelMoumeni <jackbadas01@gmail.com>
2021-06-07 20:25:16 +0200

    setting up Passport for Oauth20

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express'); 
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID : keys.googleClientID,
+    clientSecret : keys.googleSecretID,
+    callbackURL: '/auth/google/callback'
+},
+(accessToken, refreshToken, profile, cb) => { // Callback after Oauth
+console.log("access Token", accessToken);
+console.log("Profile", profile);
+}
+));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email'] // access we want from the user
+}))
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 
 const PORT = process.env.PORT || 5000; //use 5000 in our machine instead use the PORT that the cloud gives to us 
 app.listen(5000);
-// Client ID 707770149189-8ne0mk321ovem99qa5si7p0n9nhen67e.apps.googleusercontent.com
-// SECRET ID _YBVMOQ03OwDWzgBgBnqAone
